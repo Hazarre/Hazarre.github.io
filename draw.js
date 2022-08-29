b4fd764b9d4ff0983cb6b109f995374c1ce859b7
Hazarre <Hazarre@users.noreply.github.com>
2022-08-29 05:43:31 +0000

    deploy: 32195367a8d9504a0bdb7e063d34a9ccb0539706

--- draw.js
+++ draw.js
@@ -1,10 +1,13 @@
+
+let width = window.innerWidth*5/12;
+let height = width/2; 
+
+const camera = new THREE.PerspectiveCamera( 75, width/height, 0.1, 1000 );
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-scene.background = new THREE.Color( 0xffffff ); 
+scene.background = new THREE.Color( 0x000000 ); 
 
 const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#canvas')});
-renderer.setSize( window.innerWidth, window.innerHeight );
-
+renderer.setSize( width, height );
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -12,14 +15,10 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
-
 const animate = function () {
     requestAnimationFrame( animate );
-
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-
     renderer.render( scene, camera );
 };
-
 animate();
