e29060ad99926c744e3a4909d5c0b2165aaea78d
Hazarre <Hazarre@users.noreply.github.com>
2024-01-29 23:09:48 +0000

    deploy: a5122155c0b643f642474472cb310fb2aee2b6e6

--- draw.js
+++ draw.js
@@ -1,24 +1,50 @@
 
 let width = window.innerWidth*5/12;
 let height = width/2; 
+let canvas = document.querySelector('#canvas');
 
-const camera = new THREE.PerspectiveCamera( 75, width/height, 0.1, 1000 );
+var container = document.getElementById('canvas');
+var w = container.offsetWidth;
+var h = container.offsetHeight;
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x000000 ); 
+const camera = new THREE.PerspectiveCamera( 75, w/ h, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#canvas')});
-renderer.setSize( width, height );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(w, h);
+container.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
-const animate = function () {
-    requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render( scene, camera );
-};
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+
 animate();
+
+
+//Add Renderer
+
+
+// renderer = new THREE.CanvasRenderer();
+// renderer.setSize(w, h);
+
+// //Add Camera
+// camera = new THREE.PerspectiveCamera( 75, w / h, 2, 1000 );
+// camera.position.z = 400;
+
+// //Create Scene with geometry, material-> mesh
+// scene = new THREE.Scene();
+// geometry = new THREE.IcosahedronGeometry( 200, 3 );
+// material = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, wireframeLinewidth: 1 } );
+// mesh = new THREE.Mesh( geometry, material );
+// scene.add( mesh );
